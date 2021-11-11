aa7dfe37b409424a34f066fe1828706ee5c2b4c3
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2021-11-11 00:50:21 -0500

    hover finalizado, pruebas menu

--- js/burgerNav.js
+++ js/burgerNav.js
@@ -3,14 +3,18 @@ const bars = document.querySelector(".fa-bars");
 const times = document.querySelector(".fa-times");
 const btnBurg = document.getElementById('btnBurg');
 
-btnBurg.addEventListener('click', ()=>{
+// btnBurg.addEventListener('click', ()=>{
     
-    // alert("CLICK");
-    if(nav.classList.contains("see")){
-        nav.classList.remove("see");
-        bars.classList.remove("hid");
-    }else{
-        nav.classList.add("see");
-        bars.classList.add("hid")
-    }
+//     if(nav.classList.contains("see")){
+//         nav.classList.remove("see");
+//         bars.classList.remove("hid");
+//     }else{
+//         nav.classList.add("see");
+//         bars.classList.add("hid")
+//     }
+// })
+
+btnBurg.addEventListener('click', ()=>{
+    nav.classList.toggle('see');
+    bars.classList.toggle('hid');
 })
--- js/skItemHid.js
+++ js/skItemHid.js
@@ -6,27 +6,29 @@ const item4 = document.querySelector(".skItem5");
 const btnHid = document.getElementById("seeMore");
 const arItems = [item1, item2, item3, item4];
 
-btnHid.addEventListener('click', () =>{
-    cont =0;
-    arItems.forEach(item =>{
-        //if(item.classList.contains("hid")){
-        //    item.classList.remove("hid");
-        //    grid.classList.remove("dis");
-        //}else{
-        //    item.classList.add("hid")
-        //    grid.classList.add("dis")
-        //}
+// btnHid.addEventListener('click', () =>{
+//     arItems.forEach(item =>{
 
-        if(item.classList.contains("")){
-            item.classList.add("hid");
-            grid.classList.add("dis");
-            alert("falta")
-        }else{
-            item.classList.remove("hid")
-            grid.classList.remove("dis")
-        }
+//         if(item.classList.contains("")){
+//             item.classList.add("hid");
+//             grid.classList.add("dis");
+//             alert("falta")
+//         }else{
+//             item.classList.remove("hid")
+//             grid.classList.remove("dis")
+//         }
         
+//     })
+    
+// })
+
+btnHid.addEventListener('click', () =>{
+    arItems.forEach(item =>{
+
+        item.classList.toggle('hid')
+        grid.classList.toggle('dis')
     })
     
 })
+
 
